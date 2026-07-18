ec40141f3943468564941a3cddc9274c3c34446b
Deepak Yadav <yadav.deepak012@gmail.com>
2026-07-19 00:16:47 +0530

    added AZ for Q1 EC2

--- Terraform_Practise/16.Practise_Sets/Q1/main.tf
+++ Terraform_Practise/16.Practise_Sets/Q1/main.tf
@@ -1,6 +1,12 @@
+resource "random_shuffle" "az" {
+  input        = data.aws_availability_zones.available.names
+  result_count = 1
+}
+
 resource "aws_instance" "deepak_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami               = var.ami_id
+  instance_type     = var.instance_type
+  availability_zone = random_shuffle.az.result[0]
 
   tags = merge(local.common_tags, {
     Name = "Deepak-EC2"
