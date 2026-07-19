1873a8e8d9cb15d0fede5b8e1ce365a702e7592a
Deepak Yadav <yadav.deepak012@gmail.com>
2026-07-20 00:57:39 +0530

    Q5 Module VPC Added

--- Terraform_Practise/16.Practise_Sets/Q5/modules/vpc/main.tf
+++ Terraform_Practise/16.Practise_Sets/Q5/modules/vpc/main.tf
@@ -78,11 +78,11 @@ resource "aws_route_table" "public-rt" {
   )
 }
 
-resource "aws_route_table" "private-rt"{
+resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block     = var.cidr_block
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
 
