fb2e635bb6cb84ae006c18f33d43ea2697c5597e
Deepak Yadav <yadav.deepak012@gmail.com>
2026-07-19 17:39:56 +0530

    Q3 Added

--- Terraform_Practise/16.Practise_Sets/Q4/locals.tf
+++ Terraform_Practise/16.Practise_Sets/Q4/locals.tf
@@ -1,4 +1,6 @@
 locals {
+  bucket_name = "${var.bucket_name}-${var.aws_region}-${data.aws_caller_identity.current.account_id}-${random_string.suffix.result}"
+
   common_tags = {
     Environment = "Dev"
     Owner       = "Platform-Team"
--- Terraform_Practise/16.Practise_Sets/Q4/main.tf
+++ Terraform_Practise/16.Practise_Sets/Q4/main.tf
@@ -34,8 +34,7 @@ resource "aws_s3_bucket" "ec2-s3-bucket" {
   bucket = "${var.bucket_name}-${var.aws_region}-${data.aws_caller_identity.current.account_id}-${random_string.suffix.result}"
 
   tags = merge(local.common_tags, {
-    Name        = "${var.bucket_name}-${var.aws_region}-${data.aws_caller_identity.current.account_id}-${random_string.suffix.result}"
-    Environment = "Dev"
+    Name = local.bucket_name
     }
   )
 }
